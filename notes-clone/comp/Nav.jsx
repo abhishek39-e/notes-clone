3ced4fe4bf893821cac4860d2d1306ca59bd7a34
red arch <abhishekkaush39@gmail.com>
2026-01-04 20:07:12 +0530

    u

---
+++ notes-clone/comp/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './Nav.css';
+const Nav = () => {
+  return (
+    <div className='firstDiv'>
+      <h1 className='text-sky-200 flex justify-center my-3 text-5xl font-semibold'>
+        NOTES!
+      </h1>
+    </div>
+  );
+};
+
+export default Nav;
--- notes-clone/src/App.jsx
+++ notes-clone/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import Nav from '../comp/Nav';
+import Searchinput from '../comp/Searchinput';
 
 function App() {
   const [title, setTitle] = useState('');
@@ -32,35 +34,50 @@ function App() {
   };
 
   return (
-    <div style={{ padding: 20 }}>
-      <h2>Notes App</h2>
+    <div className='maindiv'>
+      {/* <Nav />
+      <Searchinput /> */}
+      <div className='notes-div w-full flex flex-col items-center p-3'>
+        <h2>Notes App</h2>
 
-      <input
-        placeholder='Title'
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <br />
-      <br />
+        <input
+          id='input-title'
+          className='p35'
+          name='title'
+          placeholder='Title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <br />
+        <br />
 
-      <textarea
-        placeholder='Content'
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-      />
-      <br />
-      <br />
+        <textarea
+          id='content-area'
+          className='p35'
+          placeholder='Content'
+          value={content}
+          name='content'
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <br />
+        <br />
 
-      <button onClick={addNotes}>Add Note</button>
-
+        <button
+          className='p35 bg-gray-700 text-gray-100 px-4 py-2 rounded hover:bg-gray-600 border border-gray-600'
+          onClick={addNotes}
+        >
+          Add Note
+        </button>
+      </div>
       <hr />
-
-      {notes.map((note) => (
-        <div key={note._id}>
-          <h4>{note.title}</h4>
-          <p>{note.content}</p>
-        </div>
-      ))}
+      <div className='shownotes flex gap-5 justify-center p-3 flex-wrap'>
+        {notes.map((note) => (
+          <div className='note py-3 px-5 bg-gray-800 rounded-md' key={note._id}>
+            <h3 className='text-lg font-semibold'>{note.title}</h3>
+            <p>{note.content}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
